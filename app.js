352e3c55cb1e3d39ca15f64ddcd0d9235c64e28c
Jamiu <hyperjay55@gmail.com>
2020-07-03 20:14:36 +0100

    adding starting project

---
+++ app.js
@@ -0,0 +1,31 @@
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1")
+const color2 = document.querySelector(".color2")
+const body = document.getElementById("gradient")
+const btn = document.querySelector("button")
+
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    const rr = Math.floor(Math.random() * 256)
+    const gg = Math.floor(Math.random() * 256)
+    const bb = Math.floor(Math.random() * 256)
+    body.style.background = `linear-gradient(to right, rgb(${r}, ${g}, ${b}), rgb(${rr}, ${gg}, ${bb}))`;
+    css.textContent = body.style.background + ";"
+}
+
+function gradientProp() {
+    body.style.background = `linear-gradient(to right,  ${color1.value}  ,  ${color2.value} )`;
+    css.textContent = body.style.background + ";"
+}
+
+function setGradient() {
+    gradientProp()
+}
+
+color1.addEventListener("input", setGradient)
+color2.addEventListener("input", setGradient)
+btn.addEventListener("click", randomColor)
+gradientProp()
